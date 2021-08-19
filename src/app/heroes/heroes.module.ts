00cd64fbe9d2a247674fba9c5f15493c98b4c7b8
David <davidsantander9@gmail.com>
2021-08-19 18:33:50 -0500

    material sidenav, toolbar, icons

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
-
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -13,11 +15,13 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     AddComponent,
     SearchComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
